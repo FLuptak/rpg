82986e979a3b4463a0ecdf3b7d8b7a2a9d66307e
regeciovad <regeciovad@gmail.com>
2015-04-28 13:22:07 +0200

    Refactoring of build (fix #135)

--- rpg/package_builder.py
+++ rpg/package_builder.py
@@ -1,4 +1,4 @@
-from getpass import getuser
+from os.path import expanduser
 from subprocess import call, check_output, PIPE, Popen, STDOUT
 from rpg.utils import copy_file
 
@@ -21,16 +21,19 @@ class PackageBuilder:
                 _ret.append(str(line).replace('\n', ''))
         return _ret
 
-    def build(self, spec_file, tarball, distro=None, arch=None):
+    def build_srpm(self, spec_file, tarball, srpm_output_path):
+        call(["rpmdev-setuptree", ""])
+        user_home = expanduser("~")
+        copy_file(str(spec_file), user_home + "/rpmbuild/SPECS/")
+        copy_file(str(tarball), user_home + "/rpmbuild/SOURCES/")
+        output = Command("rpmbuild", "-bs", user_home + "/rpmbuild/SPECS/" 
+            + spec_file.name).execute()
+        srpm_path = self._get_last_word(output)
+        Command("mv", srpm_path, srpm_output_path).execute()
+        
+    def build_rpm(self, srpm_path, distro, arch):
         """builds RPM package in mock from given spec_file and tarball,
            returns None or string in case of error occurrence"""
-        call(["rpmdev-setuptree", ""])
-        user = getuser()
-        copy_file(str(spec_file), "/home/" + user + "/rpmbuild/SPECS/")
-        copy_file(str(tarball), "/home/" + user + "/rpmbuild/SOURCES/")
-        result = check_output(
-            ["rpmbuild", "-bs", "/home/" + user + "/rpmbuild/SPECS/" + spec_file.name])
-        srpm_path = self._get_last_word(result)
         p = Popen(
             ["mock", "-r", distro, srpm_path], stdout=PIPE, stderr=STDOUT)
         line = ""
@@ -42,3 +45,9 @@ class PackageBuilder:
         result = str(self._get_last_word(logs).replace("\\n'", ""))
         if not "Finish: run" in line:
             return self._parseError(result)
+    
+    def build(self, spec_file, tarball, distro=None, arch=None):
+        """builds RPM package with build_srpm and build_rpm"""
+        srpm_output_path = expanduser("~")
+        self.build_srpm(spec_file, tarball, srpm_output_path)
+        self.build_rpm(srpm_path, distro, arch)
