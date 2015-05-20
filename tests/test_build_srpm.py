d3ca7abfe9a2eabedcfdc9faa455499ae62f918a
Jan Silhan <jsilhan@redhat.com>
2015-05-20 18:25:56 +0200

    try

--- tests/test_build_srpm.py
+++ tests/test_build_srpm.py
@@ -6,22 +6,12 @@ from rpg.command import Command
 class BuildSrpmTest(RpgTestCase):
     
     def test_build_srpm(self):
+
         base = Base()
-        base.load_plugins()
-        base.process_archive_or_dir(
-            self.test_project_dir / "hello_project/hello-1.4.tar.gz")
-        base.spec.Name = "hello"
-        base.spec.Version = "1.4"
-        base.spec.Release = "1%{?dist}"
-        base.spec.License = "GPLv2"
-        base.spec.Summary = "Hello World test program"
-        base.spec.description = "Hello World C project for testing RPG."
-        base.spec.build = "make"
-        base.spec.install = "make install DESTDIR=%{RPM_BUILD_ROOT}"
-        base.run_raw_sources_analysis()
-        base.run_patched_sources_analysis()
-        base.write_spec()
-        base._package_builder.build_srpm(base.spec_path, base.archive_path, base.base_dir)
+        base.project_name = Path("/tmp/rpgtest")
+        spec_path = test_project_dir / "hellopproject" ...
+        arhive_path = test_project_dir / "hellopproject" ...
+        base.build_srpm(spec_path, archive_path, base.srpm_path)
         self.assertTrue(base.srpm_path.exists())
         
     def TearDown():
